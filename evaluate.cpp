528cbf17258f8ba83621decf121be44b51ae4d20
Michael <michael@adroit.nz>
2024-09-28 15:31:12 +0100

    [impl] Nearly working

--- evaluate.cpp
+++ evaluate.cpp
@@ -34,15 +34,17 @@ bool evaluateExpression(int &sum, int num, Operator op, int negNum)
 
 bool evaluate(const char *expression, int &result)
 {
-    int i = 0;
-    int negNum = 1;
     Operator op = ADD;
-    int sum = 0;
     const char *ptr = expression;
+
+    int negNum = 1;
+    int sum = 0;
+    int parenStack = 0;
+    int innerResult = 0;
 
     while (*ptr != '\0')
     {
-        if (isspace(*ptr) || *ptr == '(' || *ptr == ')')
+        if (isspace(*ptr))
         {
             ++ptr; // Skip spaces and parentheses
             continue;
@@ -82,8 +84,43 @@ bool evaluate(const char *expression, int &result)
             }
         }
 
-        // Parse the number
         int num = 0;
+        if (*ptr == '(')
+        {
+            parenStack++;                // Increment the count of opening parentheses
+            const char *start = ptr + 1; // Start after the opening parenthesis
+            while (*(++ptr) != '\0')
+            {
+                if (*ptr == '(')
+                    parenStack++;
+                if (*ptr == ')')
+                {
+                    parenStack--;
+                    if (parenStack == 0)
+                        break; // Found matching closing parenthesis
+                }
+            }
+
+            if (parenStack != 0)
+            { // If there are unmatched parentheses
+                std::cerr << "Error: Mismatched parentheses!" << std::endl;
+                return false;
+            }
+
+            if (!evaluate(start, innerResult))
+            {
+                return false;
+            }
+
+            cout << "Inner result: " << innerResult << endl;
+            num = innerResult;
+            negNum = 1;
+            op = ADD;
+            ++ptr;
+            continue;
+        }
+
+        // Parse the number
         while (isdigit(*ptr))
         {
             num = num * 10 + (*ptr - '0');
@@ -96,7 +133,21 @@ bool evaluate(const char *expression, int &result)
             evaluateExpression(sum, num, op, negNum);
         }
 
+        cout << "Num  " << num << " | Operator " << op << " | NegNumber " << negNum << " | sum " << sum << endl;
+
+        if (*ptr == ')')
+        {
+            cout << sum << endl;
+            return true;
+        }
+
         negNum = 1;
+    }
+
+    if (parenStack > 0)
+    {
+        std::cerr << "Error: Mismatched parentheses!" << std::endl;
+        return false;
     }
 
     result = sum;
--- evaluate.h
+++ evaluate.h
@@ -16,14 +16,15 @@ bool evaluateExpression(int &sum, int num, Operator op, int negNum);
 
 const char *testExpressions[] = {
     " 1 + 1 - 33",               // -1
-    "2 + 11",                    // 1
-    "1 + 3",                    // 4
-    "(1 + 3) * 2",           // 8
-    "(4 / 2) + 6",           // 8
-    "4 + (12 / (1 * 2))",    // 10
-    "(1 + (12 * 2)",         // False (Bracket error)
-    "(1 + (12 * -2))",       // -23
-    "(1 + (12 - 2))",        // 11
+    // "2 + 11",                    // 1
+    // "1 + 3",                    // 4
+    // "(1 + 3) * 2",           // 8
+    "(1 + 3)",           // 4
+    // "(4 / 2) + 6",           // 8
+    // "4 + (12 / (1 * 2))",    // 10
+    // "(1 + (12 * 2)",         // False (Bracket error)
+    // "(1 + (12 * -2))",       // -23
+    // "(1 + (12 - 2))",        // 11
 };
 
 int numberOfExpressions = sizeof(testExpressions) / sizeof(testExpressions[0]);
