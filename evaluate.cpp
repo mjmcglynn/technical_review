9ac276d3e30974877af9a877e9724a6d10cad2f1
Michael <michael@adroit.nz>
2024-09-28 16:40:19 +0100

    [clean] Clean up logic. Extract methods and format style

--- evaluate.cpp
+++ evaluate.cpp
@@ -4,161 +4,146 @@
 
 using namespace std;
 
-bool evaluateExpression(int &sum, int num, Operator op, int negNum)
-{
-    switch (op)
-    {
-    case ADD:
-        sum += num * negNum;
-        break;
-    case SUBTRACT:
-        sum -= num * negNum;
-        break;
-    case MULTIPLY:
-        sum *= num * negNum;
-        break;
-    case DIVIDE:
-        if (num == 0)
-        {
-            cerr << "Error: Division by zero | ";
+Operator detectOperatorAndNegativeNumber(const char *&ptr, int &negNum, Operator currOp) {
+    switch (*ptr) { // Set the operator
+        case '*':
+            ++ptr;
+            return MULTIPLY;
+        case '/':
+            ++ptr;
+            return DIVIDE;
+        case '+':
+            ++ptr;
+            return ADD;
+        case '-':
+            if (isdigit(*(ptr + 1))) { // Handle negative number
+                negNum = -1;
+                ++ptr;
+            }
+            else { // Handle subtraction operation
+                ++ptr;
+                return SUBTRACT;
+            }
+        default:
+            return currOp;
+    }
+}
+
+bool evaluateExpression(int &sum, int num, Operator op, int negNum) {
+    switch (op) {
+        case ADD:
+            sum += num * negNum;
+            break;
+        case SUBTRACT:
+            sum -= num * negNum;
+            break;
+        case MULTIPLY:
+            sum *= num * negNum;
+            break;
+        case DIVIDE:
+            if (num == 0) {
+                cerr << "Error: Division by zero | ";
+                return false;
+            }
+            sum /= num * negNum;
+            break;
+        default:
+            cerr << "Unknown operator | ";
             return false;
-        }
-        sum /= num * negNum;
-        break;
-    default:
-        cerr << "Unknown operator | ";
-        return false;
     }
     return true;
 }
 
-bool evaluate(const char *expression, int &result)
-{
+bool evaluateParentheses(const char *&ptr, int &innerResult) {
+    int parenStack = 1;
+    const char *start = ptr + 1; // Start after the opening parenthesis
+    while (*(++ptr) != '\0') {
+
+        if (*ptr == '(') parenStack++;
+        if (*ptr == ')') {
+            parenStack--;
+            if (parenStack == 0)
+                break; // Found matching closing parenthesis
+        }
+    }
+
+    if (parenStack != 0) {
+        cerr << "Error: Mismatched parentheses | ";
+        return false;
+    }
+
+    if (!evaluate(start, innerResult)) {
+        return false;
+    }
+
+    ++ptr; // Move past the closing parenthesis
+    return true;
+}
+
+int parseNumber(const char *&ptr) {
+    int num = 0;
+    while (isdigit(*ptr)) {
+        num = num * 10 + (*ptr - '0'); // Found online - way to covert multiple chars to int
+        ++ptr;
+    }
+    return num;
+}
+
+bool evaluate(const char *expression, int &result) {
     Operator op = ADD;
     const char *ptr = expression;
 
     int negNum = 1;
     int sum = 0;
-    int parenStack = 0;
     int innerResult = 0;
 
-    while (*ptr != '\0')
-    {
-        if (isspace(*ptr))
-        {
+    while (*ptr != '\0') {
+        if (isspace(*ptr)) {
             ++ptr; // Skip spaces and parentheses
             continue;
         }
 
-        // Set the operator
-        switch (*ptr)
-        {
-        case '*':
-            op = MULTIPLY;
-            ++ptr;
-            break;
-        case '/':
-            op = DIVIDE;
-            ++ptr;
-            break;
-        case '+':
-            op = ADD;
-            ++ptr;
-            break;
-        case '-':
-            if (isdigit(*(ptr + 1))) // Handle negative number
-            {
-                negNum = -1;
-                ++ptr;
-            }
-            else // Handle subtraction operation
-            {
-                op = SUBTRACT;
-                ++ptr;
-            }
-            break;
-        }
+        op = detectOperatorAndNegativeNumber(ptr, negNum, op);
 
         int num = 0;
-        if (*ptr == '(')
-        {
-            parenStack++;                // Increment the count of opening parentheses
-            const char *start = ptr + 1; // Start after the opening parenthesis
-            while (*(++ptr) != '\0')
-            {
-                if (*ptr == '(')
-                    parenStack++;
-                if (*ptr == ')')
-                {
-                    parenStack--;
-                    if (parenStack == 0)
-                        break; // Found matching closing parenthesis
-                }
-            }
-
-            if (parenStack != 0)
-            { // If there are unmatched parentheses
-                cerr << "Error: Mismatched parentheses | ";
-                return false;
-            }
-
-            if (!evaluate(start, innerResult))
-            {
-                return false;
-            }
-            else
-            {
-                ++ptr;
-            }
-
+        bool numSet = false; // Introduced to negate zero being ignored in evaluationExression if condition - was just checking if 0
+        if (*ptr == '(') {
+            if (!evaluateParentheses(ptr, innerResult)) return false;
             num = innerResult;
+            numSet = true;
             negNum = 1;
-        }
-        else if (*ptr == ')') // Handle closing parentheses without a match
-        {
-            if (parenStack == 0)
-            {
-                cerr << "Error: Unmatched closing parenthesis | ";
-                return false;
-            }
+        } else if (*ptr == ')') { // Handle closing parentheses without a match
+            cerr << "Error: Unmatched closing parenthesis | ";
             return false;
+        } else {
+            if(!isspace(*ptr)) { // A leading space will cause the number to be 0
+                num = parseNumber(ptr); // Parse the number
+                numSet = true;
+            }
         }
 
-        // Parse the number
-        while (isdigit(*ptr))
-        {
-            num = num * 10 + (*ptr - '0');
-            ++ptr;
+        if (numSet || negNum == -1) { // Check if the number is valid or it's a negative number
+            if (!evaluateExpression(sum, num, op, negNum)) return false; // Error - divide by zero or unknown operator
         }
 
-        if (num != 0 || negNum == -1)
-        { // Check if the number is valid or it's a negative number
-            evaluateExpression(sum, num, op, negNum);
-        }
-
-        if (*ptr == ')')
-        {
+        if (*ptr == ')') { // Close open parentheses
             result = sum;
             return true;
         }
+
         negNum = 1;
     }
-
     result = sum;
     return true;
 }
 
 int main()
 {
-    for (int i = 0; i < numberOfExpressions; ++i)
-    {
+    for (int i = 0; i < numberOfExpressions; ++i) {
         int result = 0;
-        if (evaluate(testExpressions[i], result))
-        {
+        if (evaluate(testExpressions[i], result)) {
             cout << "Result: " << result << "\t|\tTest Expression " << i + 1 << ": " << testExpressions[i] << endl;
-        }
-        else
-        {
+        } else {
             cout << "Invalid expression: " << testExpressions[i] << endl;
         }
     }
--- evaluate.h
+++ evaluate.h
@@ -4,36 +4,40 @@
 #include <stdbool.h>
 
 enum Operator {
-    ADD,        
-    SUBTRACT,   
-    MULTIPLY,  
-    DIVIDE      
+    ADD,
+    SUBTRACT,
+    MULTIPLY,
+    DIVIDE
 };
 
-
+Operator detectOperatorAndNegativeNumber(const char *&ptr, int &negNum, Operator currOp);
 bool evaluate(const char *expression, int &result);
 bool evaluateExpression(int &sum, int num, Operator op, int negNum);
+bool evaluateParentheses(const char *&ptr, int &innerResult);
+int parseNumber(const char *&ptr);
 
 const char *testExpressions[] = {
-    "1 + 1 - 33",               // -31
-    " 2 + 11",                  // 13
-    "-1 +2",                    // 1
-    "1 + 3",                    // 4
-    "(1 + 3) * 2",              // 8
-    "(4 / 2) + 6",              // 8
-    "4 + (12 / (1 * 2))",       // 10
-    "(1 + (12 * 2))",           // 25
-    "(1 + (12 * -2))",          // -23
-    "(1 + (12 - 2))",           // 11
-    "1 + (2 * (1 + 1)) + 1",    // 6
-    "1 + (2 * (1 + 1)",         // Error
-    "(1 + (2 * (3 + 4))) * 2",   // 30
-    "1 + )",                     // Error
-    "1 + -)",                     // Error
-    "(1 + -)"                     // Error
+    "1 + 1 - 33",              // -31
+    " 2 + 11",                 // 13
+    "-1 +2",                   // 1
+    "1 + 3",                   // 4
+    "(1 + 3) * 2",             // 8
+    "(4 / 2) + 6",             // 8
+    "4 + (12 / (1 * 2))",      // 10
+    "(1 + (12 * 2))",          // 25
+    "(1 + (12 * -2))",         // -23
+    "(1 + (12 - 2))",          // 11
+    "1 + (2 * (1 + 1)) + 1",   // 6
+    "(1 + (2 * (3 + 4))) * 2", // 30
+    "(1 - 1)" ,                // 0
+    "0 + 1 + 1",                // 2
+    "1111111 + 1111111",       // 2222222
+    "1 + )",                   // Error
+    "1 + -)",                  // Error
+    "1/0",                     // Error
+    "1 + (2 * (1 + 1)"        // Error
 };
 
 int numberOfExpressions = sizeof(testExpressions) / sizeof(testExpressions[0]);
-
 
 #endif // EVALUATE_H
